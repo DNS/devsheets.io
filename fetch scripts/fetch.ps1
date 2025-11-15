12c4d7626d575a0e42918aa0e5770a5d6460cd55
DNS <37093+DNS@users.noreply.github.com>
2025-11-15 22:39:12 +0700

    -

---
+++ fetch scripts/fetch.ps1
@@ -0,0 +1,18 @@
+https://www.devsheets.io/sheets?technology=TypeScript
+https://www.devsheets.io/sheets?technology=Vue.js
+https://www.devsheets.io/sheets?technology=SQL
+https://www.devsheets.io/sheets?technology=Linux
+https://www.devsheets.io/sheets?technology=Kotlin
+https://www.devsheets.io/sheets?technology=Next.js
+https://www.devsheets.io/sheets?technology=Nginx
+https://www.devsheets.io/sheets?technology=MongoDB
+https://www.devsheets.io/sheets?technology=MySQL
+https://www.devsheets.io/sheets?technology=Node.js
+https://www.devsheets.io/sheets?technology=NPM
+https://www.devsheets.io/sheets?technology=PostgreSQL
+https://www.devsheets.io/sheets?technology=Python
+
+
+1..6 | %{ $(iwr "https://www.devsheets.io/sheets?page=$_").Links.href } > links.txt
+gc ./links.txt | sls /sheets/ | %{ 'https://www.devsheets.io' + $_ } > links2.txt
+
